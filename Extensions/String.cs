366e9ba5d027a88a46ba87680f87cc9c769f56a5
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-03-10 09:21:16 +0000

    Added String Extensions

---
+++ Extensions/String.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class StringExtension
+    {
+        public static string RemoveLastCharacter(this String @string)
+        {
+            return @string.Substring(0, @string.Length - 1);
+        }
+        public static string RemoveLast(this String @string, int number)
+        {
+            return @string.Substring(0, @string.Length - number);
+        }
+        public static string RemoveFirstCharacter(this String @string)
+        {
+            return @string.Substring(1);
+        }
+        public static string RemoveFirst(this String @string, int number)
+        {
+            return @string.Substring(number);
+        }
+    }
+}
