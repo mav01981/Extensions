ff7c0bfd0b79c156d183c49913110b0fbfe1e599
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-03-16 15:31:23 +0000

    Tidy up

--- Extensions/CSV.cs
+++ Extensions/CSV.cs
@@ -11,93 +11,86 @@ namespace Extensions
         {
             StringBuilder csv = new StringBuilder();
 
-            try
+            if (rows != null)
             {
-                if (rows != null)
+                foreach (var header in rows)
                 {
-                    foreach (var header in rows)
-                    {
-                        var properties = header.GetType().GetProperties();
+                    var properties = header.GetType().GetProperties();
 
-                        foreach (var prop in properties)
+                    foreach (var prop in properties)
+                    {
+                        if (prop.PropertyType == typeof(string) || prop.PropertyType == typeof(int))
                         {
-                            if (prop.PropertyType == typeof(string) || prop.PropertyType == typeof(int))
-                            {
-                                csv = csv.AppendFormat($"{prop.Name}{separator}");
-                            }
-                            else if (typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
-                            {
-                                csv = csv.AppendFormat($"{prop.Name}{separator}");
-                            }
+                            csv = csv.AppendFormat($"{prop.Name}{separator}");
                         }
-                        break;
-                    }
-                    csv.AppendLine();
-
-                    foreach (var row in rows)
-                    {
-                        var properties = row.GetType().GetProperties();
-
-                        foreach (var prop in properties)
+                        else if (typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
                         {
-                            object value = GetPropValue(row, prop.Name);
+                            csv = csv.AppendFormat($"{prop.Name}{separator}");
+                        }
+                    }
+                    break;
+                }
+                csv.AppendLine();
 
-                            if (value == null)
-                            {
-                                csv = csv.AppendFormat($"#{separator}");
-                            }
-                            else if (value.GetType() == typeof(string) || value.GetType() == typeof(int))
-                            {
-                                csv = csv.AppendFormat($"{value}{separator}");
-                            }
-                            else if (typeof(IEnumerable).IsAssignableFrom(value.GetType()))
-                            {
-                                string split = string.Empty;
+                foreach (var row in rows)
+                {
+                    var properties = row.GetType().GetProperties();
 
-                                var listOfItems = value as IList;
+                    foreach (var prop in properties)
+                    {
+                        object value = GetPropValue(row, prop.Name);
 
-                                foreach (var t in Flatten(listOfItems))
+                        if (value == null)
+                        {
+                            csv = csv.AppendFormat($"#{separator}");
+                        }
+                        else if (value.GetType() == typeof(string) || value.GetType() == typeof(int))
+                        {
+                            csv = csv.AppendFormat($"{value}{separator}");
+                        }
+                        else if (typeof(IEnumerable).IsAssignableFrom(value.GetType()))
+                        {
+                            string split = string.Empty;
+
+                            var listOfItems = value as IList;
+
+                            foreach (var t in Flatten(listOfItems))
+                            {
+                                Type itemType = value.GetType().GetGenericArguments()[0];
+
+                                if (itemType == typeof(int))
                                 {
-                                    Type itemType = value.GetType().GetGenericArguments()[0];
+                                    split = split + $"{t} |";
+                                }
+                                else
+                                {
+                                    var items = t.GetType().GetProperties();
 
-                                    if (itemType == typeof(int))
+                                    foreach (var i in items)
                                     {
-                                        split = split + $"{t} |";
-                                    }
-                                    else
-                                    {
-                                        var items = t.GetType().GetProperties();
+                                        object val = GetPropValue(t, i.Name);
 
-                                        foreach (var i in items)
+                                        var isList = val as IList;
+
+                                        if (isList == null)
                                         {
-                                            object val = GetPropValue(t, i.Name);
-
-                                            var isList = val as IList;
-
-                                            if (isList == null)
+                                            split = split + $"{val} |";
+                                        }
+                                        else
+                                        {
+                                            foreach (var v in isList)
                                             {
-                                                split = split + $"{val} |";
-                                            }
-                                            else
-                                            {
-                                                foreach (var v in isList)
-                                                {
-                                                    split = split + $"{v} |";
-                                                }
+                                                split = split + $"{v} |";
                                             }
                                         }
                                     }
                                 }
-                                csv = csv.AppendFormat($"{split}{separator}");
                             }
+                            csv = csv.AppendFormat($"{split}{separator}");
                         }
-                        csv.AppendLine();
                     }
+                    csv.AppendLine();
                 }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
             }
             return csv.ToString();
         }
