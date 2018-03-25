5c69bbbd6b37e5b6d1d01b5380aaabf5a2dd8636
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-03-25 10:27:44 +0100

    Added Enum Extension and XML Extension

---
+++ Tests/EnumTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Tests
+{
+    using Xunit;
+    using Extensions;
+
+    enum Job
+    {
+        PoliceMan,
+        Salesman,
+        Teacher,
+        Gardener
+    }
+    public class EnumTests
+    {
+        [Fact]
+        public void Enum_Has_TypeAndValue()
+        {
+            Assert.True(Job.Gardener.Has(0));
+        }
+        [Fact]
+        public void Enum_HasNot_TypeAndValue()
+        {
+            Assert.False(Job.Gardener.Has("Test"));
+        }
+        [Fact]
+        public void Enum_Is_Value()
+        {
+            Assert.True(Job.Gardener.Is(3));
+        }
+        [Fact]
+        public void Enum_IsNot_Value()
+        {
+            Assert.False(Job.Gardener.Is("Test"));
+        }
+    }
+}
