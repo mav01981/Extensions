955f1be2d6350672e45f75d7b5d74c1f98ee636d
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-04-21 13:08:48 +0100

    Add Flatten Model Extension

---
+++ Extensions/ObjectExtension.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extensions
+{
+    public static class ObjectExtension
+    {
+        private static string FlattenModel(Type type, object @object, ref string output)
+        {
+            var properties = type.GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (property.PropertyType.IsValueType || property.PropertyType == typeof(string))
+                {
+                    output = output + property.Name + ":" + property.GetValue(@object, null).ToString() + " ";
+                }
+                else if (property.PropertyType.IsArray)
+                {
+                    if (property.PropertyType.GetElementType().IsValueType || property.PropertyType.GetElementType() == typeof(string))
+                    {
+                        IEnumerable items = property.GetValue(@object) as IEnumerable;
+                        output = output + property.Name + ":";
+
+                        foreach (var item in items.Enum())
+                        {
+                            output = output + item.ToString();
+                        }
+                    }
+                    else
+                    {
+                        IEnumerable items = property.GetValue(@object) as IEnumerable;
+
+                        foreach (var item in items.Enum())
+                        {
+                            if (item != null)
+                            {
+                                output = output + "|" + item.GetType().ToString() + "|";
+                                FlattenModel(item.GetType(), item, ref output);
+                            }
+                        }
+                    }
+                }
+                else if (property.GetValue(@object) is IList && property.GetValue(@object).GetType().IsGenericType)
+                {
+                    IEnumerable items = property.GetValue(@object) as IEnumerable;
+
+                    foreach (var item in items.Enum())
+                    {
+                        if (item != null)
+                        {
+                            output = output + "|" + item.GetType().ToString() + "|";
+                            FlattenModel(item.GetType(), item, ref output);
+                        }
+                    }
+                }
+                else
+                {
+                    if (property.GetValue(@object, null) != null)
+                    {
+                        output = output + "|" + type.ToString() + "|";
+                        FlattenModel(property.PropertyType, property.GetValue(@object, null), ref output);
+                    }
+                }
+            }
+            return string.Empty;
+        }
+        /// <summary>
+        /// Flatten Object to String.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="object"></param>
+        /// <returns></returns>
+        public static string ObjectToString<T>(this T @object)
+        {
+            string output = string.Empty;
+
+            Type type = @object.GetType();
+
+            var properties = type.GetProperties();
+
+            FlattenModel(type, @object, ref output);
+
+            return output;
+        }
+
+        public static IEnumerable Enum(this IEnumerable @objectArray)
+        {
+            if (@objectArray != null)
+            {
+                foreach (var t in @objectArray)
+                {
+                    yield return t;
+                }
+            }
+            else
+            {
+                yield break;
+            }
+        }
+    }
+}
---
+++ Tests/ObjectExtensionTests.cs
@@ -0,0 +1,155 @@
+﻿namespace Tests
+{
+    using Extensions;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class CarsLots
+    {
+        public List<Car> Lots { get; set; }
+    }
+    public class CarsArray
+    {
+        public Car[] Lots { get; set; }
+    }
+
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public Engine Engine { get; set; }
+        public List<InsuredDriver> InsuredDriver { get; set; }
+        public int[] Ids { get; set; }
+        public InsuredDriver[] PreviousDriverrs { get; set; }
+    }
+    public class Engine
+    {
+        public string EngineSize { get; set; }
+        public Type Measure { get; set; }
+
+    }
+    public class Type
+    {
+        public string Name { get; set; }
+    }
+    public class InsuredDriver
+    {
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+
+    public class ObjectExtensionTests
+    {
+        [Fact]
+        public void Flatten_Model_ToString()
+        {
+            var vehicle = new Car()
+            {
+                Ids = new int[] { 1, 2, 3 },
+
+                Name = "Nissan Primera 1.8 Flare 5dr",
+                Colour = "Red",
+                Engine = new Engine()
+                {
+                    EngineSize = "1.8L",
+                    Measure = new Type()
+                    {
+                        Name = "New measure Type"
+                    }
+                }
+            };
+
+            var output = vehicle.ObjectToString<Car>();
+
+            Assert.Equal("Name:Jon Doe Age:34 Address:Tests.Address ", output);
+        }
+
+        [Fact]
+        public void Flatten_ModelWithList_ToString()
+        {
+            var lots = new CarsLots()
+            {
+                Lots = new List<Car>()
+                {
+                    new Car()
+                    {
+                        Ids = new int[] { 1, 2, 3 },
+
+                        Name = "Nissan Primera 1.8 Flare 5dr",
+                        Colour = "Red",
+                        Engine = new Engine()
+                        {
+                            EngineSize = "1.8L",
+                            Measure = new Type()
+                            {
+                                Name = "New measure Type"
+                            }
+                        },
+                        InsuredDriver = new List<InsuredDriver>()
+                        {
+                            new InsuredDriver()
+                            {
+                                FullName = "Jonathan Smart",
+                                EmailAddress = "mail@mailaddrees.com"
+                            }
+                        },
+                        PreviousDriverrs = new InsuredDriver[]
+                        {
+                              new InsuredDriver()
+                            {
+                                FullName = "Jonathan Smart",
+                                EmailAddress = "mail@mailaddrees.com"
+                            },
+                                   new InsuredDriver()
+                            {
+                                FullName = "Jonathan Smart",
+                                EmailAddress = "mail@mailaddrees.com"
+                            }
+
+                        }
+                    },
+                    new Car()
+                    {
+                        Ids = new int[] { 1, 2, 3 },
+                        Name = "Nissan Primera 1.8 Flare 5dr",
+                        Colour = "Red",
+                        Engine = new Engine()
+                        {
+                            EngineSize = "1.8L",
+                            Measure = new Type()
+                            {
+                                Name = "New measure Type"
+                            }
+                        }
+                    }
+
+                }
+
+
+            };
+
+            var output = lots.ObjectToString<CarsLots>();
+
+            Assert.Equal("Name:Jon Doe Age:34 Address:Tests.Address ", output);
+        }
+
+        [Fact]
+        public void Flatten_ModelWithArray_ToString()
+        {
+            var array = new CarsArray()
+            {
+                Lots = new Car[]
+                {
+                  new Car()
+                  {
+
+                  }
+              }
+            };
+
+            //var output = lots.ObjectToString<CarsLots>();
+
+            //Assert.Equal("Name:Jon Doe Age:34 Address:Tests.Address ", output);
+        }
+    }
+}
