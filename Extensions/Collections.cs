4989092d15ef0a6722f85b4566ecd48f2b4612e1
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-08-14 14:51:40 +0100

    Added new SWAP method

--- Extensions/Collections.cs
+++ Extensions/Collections.cs
@@ -30,5 +30,13 @@ namespace Extensions
         {
             return value1.Except(value2);
         }
+        public static IList<T> Swap<T>(this IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+
+            return list;
+        }
     }
 }
