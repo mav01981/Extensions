00e2377acf22149207b5c7b10bdbdb3d06875675
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-09-09 09:58:54 +0100

    Added Sharepoint and Outlook Extensons.

---
+++ MicrosoftExtensions/Outlook.cs
@@ -0,0 +1,28 @@
+﻿namespace MicrosoftExtensions
+{
+    using Microsoft.Office.Interop.Outlook;
+
+    public static class OutlookExtensions
+    {
+        public static string FormatEmailAddress(this MailItem mail)
+        {
+            AddressEntry sender = mail.Sender;
+            string SenderEmailAddress = string.Empty;
+
+            if (sender.AddressEntryUserType == OlAddressEntryUserType.olExchangeUserAddressEntry || sender.AddressEntryUserType == OlAddressEntryUserType.olExchangeRemoteUserAddressEntry)
+            {
+                ExchangeUser exchUser = sender.GetExchangeUser();
+                if (exchUser != null)
+                {
+                    SenderEmailAddress = exchUser.PrimarySmtpAddress;
+                }
+            }
+            else
+            {
+                SenderEmailAddress = mail.SenderEmailAddress;
+            }
+
+            return SenderEmailAddress;
+        }
+    }
+}
---
+++ MicrosoftExtensions/SharePoint.cs
@@ -0,0 +1,55 @@
+﻿namespace MicrosoftExtensions
+{
+    using Microsoft.SharePoint.Client;
+    using System;
+
+    public static class SharePointExtensions
+    {
+        public static bool FolderExists(this ClientContext context, string filePath)
+        {
+            try
+            {
+                var folder = context.Web.GetFolderByServerRelativeUrl(filePath);
+                context.Load(folder);
+                context.ExecuteQuery();
+                return true;
+            }
+            catch (ServerException ex)
+            {
+                if (ex.ServerErrorTypeName == "System.IO.DirectoryNotFoundException")
+                {
+                    return false;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool FileExists(this ClientContext context, string fileUrl)
+        {
+            //"/sites/Documents/MyFile.docx"
+            File checkFile = context.Web.GetFileByServerRelativeUrl(fileUrl);
+            context.Load(checkFile, fe => fe.Exists);
+            context.ExecuteQuery();
+
+            return checkFile.Exists;
+        }
+
+        public static void CreateFolder(this ClientContext context, string root, string folder)
+        {
+            List docs = context.Web.Lists.GetByTitle(root);
+            docs.EnableFolderCreation = true;
+            docs.RootFolder.Folders.Add(folder);
+            context.ExecuteQuery();
+        }
+
+        public static void DeletFolder(this ClientContext context, string folder)
+        {
+            context.Web.GetFileByServerRelativeUrl(folder).DeleteObject();
+            context.ExecuteQuery();
+        }
+    }
+}
